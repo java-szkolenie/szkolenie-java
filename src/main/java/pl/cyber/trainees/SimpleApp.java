8f4eeb0b6fd15d1c21b445a16a9bfbaac50e11af
Mariusz Tański <mariusz.tanski@programisci.eu>
2022-07-02 11:27:39 +0200

    slownik-pojec.txt

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,7 +1,43 @@
 package pl.cyber.trainees;
 
+import java.math.BigDecimal;
+
 public class SimpleApp {
+
     public static void main(String[] args) {
+
+        /*
+        1. Klasa -> Class
+        2. Obiekt -> Object
+        3. Zmienna -> Variables
+            typy, deklaracja zmiennych
+         */
+
+
+//      Ctrl+ lewy przycisk myszy na 'String'
+        String tekst = "";  //-> var string = "" -> string s = ""
+
+        BigDecimal value = BigDecimal.valueOf(1);
+        BigDecimal value1 = BigDecimal.ONE;
+
+
+        // Program wyświetlający
+        // w terminalu przykładowy tekst
+
+//        System.out.println("My first Application");
         System.out.println("My first Application");
+        System.out.println("Get BigDecimal value:" + value + " valkue1: " + value1);
+
+/*
+ Ctrl+/
+tekst
+tekst
+tekst
+tekst
+tekst
+tekst
+*/
+
+
     }
 }
