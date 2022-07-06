6c4605a18e82276da730bf882235c2fd4e272b68
Mariusz Tański <mariusz.tanski@programisci.eu>
2022-07-06 12:14:47 +0200

    metoda na zaokrąglanie liczb

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,12 +1,15 @@
 package pl.cyber.trainees;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class SimpleApp {
 
     enum Stan {on, off};
     enum Plec {kobieta, mezczyzna};
     public static void main(String[] args) {
+
+        int scale = 0;
 
 //praca domowa
         //Utworzenie 2 zmiennych typu String i ich inicjalizacja
@@ -16,6 +19,18 @@ public class SimpleApp {
         //dodawanie , odejmowanie, monożenie i dzielenie.
         //proszę o wykonanie działania dzielenie przez 0
 
+        roundNumber("2.34", scale);
+        System.out.println("NEXT");
+        roundNumber("2.44", scale);
+        System.out.println("NEXT");
+        roundNumber("2.46", scale);
+        System.out.println("NEXT");
+        roundNumber("2.5", scale);
+        System.out.println("NEXT");
+        roundNumber("2.55", scale);
+        System.out.println("NEXT");
+        roundNumber("2.6", scale);
+        System.out.println("NEXT");
     }
 
     public static int add(int l1, int l2) {
@@ -28,6 +43,22 @@ public class SimpleApp {
     public static String concat(String l1, String l2) {
         var result = l1 + l2 + "\n";
         return result;
+    }
+
+    public static void roundNumber(final String number, int scale) {  //To najczęstszy sposób gdzie ja stosuję zaokrąglanie liczb
+        BigDecimal value = BigDecimal.valueOf(Double.parseDouble(number));
+        var roundHalfUp = value;
+        roundHalfUp = roundHalfUp.setScale(scale, RoundingMode.UP);
+
+        var roundHalfDown = value;
+        roundHalfDown = roundHalfDown.setScale(scale, RoundingMode.DOWN);
+
+        var roundHalfFloor = value;
+        roundHalfUp = roundHalfUp.setScale(scale, RoundingMode.FLOOR);
+
+        System.out.println("Round Half up: " + roundHalfUp);
+        System.out.println("Round Half down: " + roundHalfDown);
+        System.out.println("Round Half floor: " + roundHalfFloor);
     }
 }
 
